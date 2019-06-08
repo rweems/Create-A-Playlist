149bfb2e3eab662019b0815cf323aeb53b1b5734
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-06-08 17:03:34 -0400

    created views

--- playlist_app/views.py
+++ playlist_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import User, Playlist, Song
+from .serializer import UserSerializer, PlaylistSerializer, SongSerializer
+from rest_framework import viewsets
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+    
