1ae4a3944af0315854a052d23b33bdaadbc9dab3
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-06-08 13:42:57 -0400

    created models

--- playlist_app/models.py
+++ playlist_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Playlist(models.Model):
+    playlistName = models.CharField(max_length=100) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='playlists')
+
+    def __str__(self):
+        return self.playlistName
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='songs')
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='songs')
