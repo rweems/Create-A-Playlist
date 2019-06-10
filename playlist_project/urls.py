acb8c15a89acd7b0e6c4f4d6da54c5c0508bb593
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-06-10 10:54:25 -0400

    connect urls to root

--- playlist_project/urls.py
+++ playlist_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('playlist_app.urls'))
+     path('', views.FrontendAppView.as_view()),
+    path('api/v1/',include('playlist_app.urls'))
 ]
