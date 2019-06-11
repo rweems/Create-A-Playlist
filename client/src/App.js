01b8dcc793396891c467f7118f92ca5b014e041a
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-06-11 11:47:29 -0400

    create routes for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import UserList from "./components/UserList";
+import User from "./components/User";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ class App extends Component {
 
           <Switch>
             <Route exact path="/" component={UserList} />
+            <Route path="/users/:id" component={User} />
 
           </Switch>
         </div>
