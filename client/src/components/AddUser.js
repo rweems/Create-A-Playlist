7b78295a3003e48d28a6b00d3657bbb3f05cc5c7
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-06-11 11:48:33 -0400

    created add user component

---
+++ client/src/components/AddUser.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+class AddUser extends Component {
+    state = {}
+    render() {
+        return (
+            <form onSubmit={this.props.createUser}>
+                <div>
+                    <label htmlFor='name'>Name: </label>
+                    <input id='name' type='text'
+                        name='name'
+                        placeholder='Name'
+                        onChange={this.props.handleChange}
+                        value={this.props.newUser.name} />
+                </div>
+
+                <div>
+                    <label htmlFor='email'>Email: </label>
+                    <input id='email' type='text'
+                        name='email'
+                        placeholder='Email'
+                        onChange={this.props.handleChange}
+                        value={this.props.newUser.email} />
+                </div>
+
+                <div>
+                    <label htmlFor='age'>Age: </label>
+                    <input id='age' type='number'
+                        name='age'
+                        placeholder='Age'
+                        onChange={this.props.handleChange}
+                        value={this.props.newUser.age} />
+                </div>
+
+                <br />
+                <div>
+                    <input type='submit' value='Submit' />
+                </div>
+            </form>
+        );
+    }
+}
+
+
+export default AddUser;
