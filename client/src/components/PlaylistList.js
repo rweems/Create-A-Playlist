f17abc26cf18fe83de5f396d3f676915dc9c5a87
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-06-12 10:11:43 -0400

    created playlist list component

---
+++ client/src/components/PlaylistList.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import AddUser from './AddUser'
+
+class PlaylistList extends Component {
+    state = {
+        error: '',
+        playlists: [],
+        newPlaylist: {
+            playlistName: '',
+        },
+        isFormDisplayed: false
+    }
+
+    componentDidMount = () => {
+        axios.get('api/v1/playlists/').then(res => {
+            this.setState({ playlists: res.data });
+        })
+    }
+
+    toggleForm = () => {
+        this.setState((state, props) => {
+            return ({ isFormDisplayed: !state.isFormDisplayed })
+        })
+    }
+
+    handleChange = (e) => {
+        const createdPlaylist = { ...this.state.newPlaylist }
+        createdPlaylist[e.target.name] = e.target.value
+        this.setState({ newPlaylist: createdPlaylist })
+
+    }
+
+    createPlaylist = (e) => {
+        e.preventDefault()
+        axios.post('/api/v1/playlists/', this.state.newPlaylist)
+            .then(res => {
+                const playlistList = [...this.state.playlists]
+                playlistList.push(res.data)
+                this.setState({
+                    newPlaylist: {
+                        playlistName: ''
+                    },
+                    isFormDisplayed: false,
+                    playlists: playlistList
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Playlists</h1>
+                {
+                    this.state.playlists.map((playlist) => {
+                        return (
+                            <div key={playlist.id} className="linkTo">
+                                <Link to={`api/v1/playlists/${playlist.id}`} style={{ paddingTop: '10px' }} name={this.state.playlist}>
+                                    {playlist.playlistName}
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+
+                <button onClick={this.toggleForm} className="buttonClass">New Playlist?</button>
+                {
+                    this.state.isFormDisplayed
+                        ?
+                        <form onSubmit={this.createPlaylist}>
+                            <div>
+                                <label htmlFor='playlistName'>Playlist Name: </label>
+                                <input id='playlistName' type='text'
+                                    name='playlistName'
+                                    placeholder='Playlist Name'
+                                    onChange={this.handleChange}
+                                    value={this.state.newPlaylist.playlistName} />
+                            </div>
+                            <br />
+                            <div>
+                                <input type='submit' value='Submit' />
+                            </div>
+                        </form> : null
+                }
+
+            </div>
+        )
+    }
+}
+
+export default PlaylistList;
